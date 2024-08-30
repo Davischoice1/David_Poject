83c8e5f3ae9647ad578fbf2b51039e86ed9392a0
DavischoiceOmeiza <120713704+Davischoice1@users.noreply.github.com>
2024-08-30 04:33:09 +0100

    Update Tomato_app.py

--- Tomato_app.py
+++ Tomato_app.py
@@ -390,8 +390,8 @@ if st.session_state.logged_in:
                 predicted_class, confidence, disease_solution = predict(model, img)
                 
                 if predicted_class:
-                    st.success(f"Prediction: {predicted_class} ({confidence}% confidence)")
-                    st.info(disease_solution)
+                        st.markdown(f"<span style='color:ffffff;'>Prediction:</span> <span style='color:ffffff;'>{predicted_class}</span> <span style='color:#ffffff;'>({confidence}% confidence)</span>", unsafe_allow_html=True)
+                        st.markdown(f"<span style='color:#ffffff;'>Disease:</span><br><span style='color:#ffffff;'>{disease_solution}</span><span style='color:#ffffff;'>:</span>", unsafe_allow_html=True)
 
                     # Save the image and prediction to the database
                     img_byte_arr = io.BytesIO()
